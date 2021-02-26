37610c5a0275e0ebef651ed605fda528c08b5cb9
kramar-alexandr <kramar.alexandr@gmail.com.com>
2021-02-26 17:07:21 +0200

    update

--- bakuSERPbot.js
+++ bakuSERPbot.js
@@ -1,14 +1,32 @@
+require('dotenv').config()
 const { Telegraf } = require('telegraf')
-const token = '1527680518:AAH8zOl7H0Z5wz4I_EPOZ9hhG6YEUxZewoE';
+//const token = '1612585342:AAEpyJGuMUTA7XQcyW4JU2JVlIIKa4y74NU';
+const readLastLines = require('read-last-lines');
+const logpath = '';
 
-//const bot = new Telegraf(process.env.BOT_TOKEN)
-const bot = new Telegraf(token)
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+//const bot = new Telegraf(token)
 bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.hears('log', (ctx) => getLasthansaLog(ctx))
+bot.command('log', (ctx) => getLasthansaLog(ctx))
 bot.launch()
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
+
+function getLasthansaLog(ctx){
+    ctx.reply('res');
+    readLastLines.read(process.env.LOG_PATH, 50)
+	.then((lines) => {
+        ctx.reply(lines);
+        });
+    //readLastLines.read(process.env.LOG_PATH, 50, (lines) => {
+    //    console.log(lines);
+    //});
+	//.then((lines,ctx) => ctx.reply(lines));
+}
